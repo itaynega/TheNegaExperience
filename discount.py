baafd9155e37a12d5f74032820e2fea48c3f2c3b
itaynega <93132840+itaynega@users.noreply.github.com>
2021-12-15 17:33:35 +0200

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,23 +1,41 @@
 from collections import defaultdict
+from math import ceil
 
+
+# getting discount percentage from user
+while True:
+    try:
+        discount_percentage = int(input("Discount percentage: "))
+        if 0 <= discount_percentage < 100:
+            break
+        else:
+            print("error. try again.")
+    except ValueError:
+        print("error. try again.")
+    discount_percentage_multiplier = 1 - discount_percentage/100
+
+
+# splitting shared meals between diners
 prices_and_names = defaultdict(int)
-discount_percentage = 1 - int(input("discount percentage: "))/100
-if input("Any shared meals? type 'y' for yes. \n>> ").lower() == 'y':
-    more = True
-    while more:
-        shared_meals = input("shared meals - write all name and price at the end. \n"
-                             "example: itay dan harel 79. \n>> ").split()
-        shared_price = int(shared_meals[-1])
-        del(shared_meals[-1])
-        for name in shared_meals:
-            prices_and_names[name] += round(shared_price/3)
+if input("Any shared meals? type 'y' for Yes, any other key for No. \n>> ").lower() == 'y':
+    try:
+        shared_meal = input("shared meal - write all names, and then the price at the end. \n"
+                             "example: itay dan harel 79. press enter to end \n>> ").split()
+        for char in shared_meal:
+            
+        names = [x for x in shared_meals[:-1] if x.isalpha()]
+        shared_price = ceil(int(shared_meals[-1])/len(names))
+        for name in names:
+            prices_and_names[name] += shared_price
         print(prices_and_names.items())
-        more = bool(int(input("are there more shared meals? 1 for y, 0 for no\n>>")))
+    except ValueError:
+        print("value error")
+
 
 
 prices_before_discount = input("price before discount - name + price: ").split()
 for i in range(len(prices_before_discount)):
-    if i % 2 != 1:
+    if i%2 != 1:
         prices_and_names[prices_before_discount[i]] += int(prices_before_discount[i + 1])
 
 for name, price in prices_and_names.items():
@@ -25,5 +43,4 @@ for name, price in prices_and_names.items():
     price_with_tip = price_after_discount + price*0.1
     print(f"For {name}: Original price: {price} ==> After discount: {price_after_discount.__round__()}"
           f" ==> With tip: {price_with_tip.__round__()}")
-
 
